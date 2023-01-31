de40a4408fa61022a354aab4d907fb3d04bf9ece
Eric Pineda <ericvpineda@berkeley.edu>
2023-01-31 13:24:27 -0800

    updated -- implemented pass setHours and setMinutes fxn from UserInput to Time comp

--- src/Components/UserInput/Time/Time.tsx
+++ src/Components/UserInput/Time/Time.tsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import {FC, ChangeEvent} from 'react'
 
-const Time: React.FC = () => {
+interface TimeValues {
+    setHours: (param: number) => void,
+    setMinutes: (param: number) => void
+}
+
+const Time: FC<TimeValues> = (props): JSX.Element => {
+    const setHoursHandler = (event: ChangeEvent<HTMLInputElement>) => {
+        props.setHours(+event.target.value)
+    }
+    
+    const setMinutesHandler = (event: ChangeEvent<HTMLInputElement>) => {
+        props.setMinutes(+event.target.value)
+    }
+
     return (
-        <div></div>
+        <div className='d-flex flex-row'>
+            <label className='form-label' htmlFor="hours">Hours</label>
+            <input onChange={setHoursHandler} id="hours" className='form-control' type="number" min="0" max="24"/>
+            <label className='form-label' htmlFor="minutes">Minutes</label>
+            <input onChange={setMinutesHandler} id="minutes" className='form-control' type="number" min="0" max="59"/>
+        </div>
     )
 }
 
