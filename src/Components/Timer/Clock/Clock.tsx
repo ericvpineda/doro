40968de7575a8d149cc398873d99830cb643a1dc
Eric Pineda <ericvpineda@berkeley.edu>
2023-02-01 15:06:05 -0800

    updated -- implemented basic start/stop button

--- src/Components/Timer/Clock/Clock.tsx
+++ src/Components/Timer/Clock/Clock.tsx
@@ -1,72 +1,56 @@
-import React, {FC, useEffect, useState} from 'react'
+import React, {FC, useEffect, useState, MouseEvent} from 'react'
 import styles from './Clock.module.css'
 
-const Clock: FC = (): JSX.Element => {
+const Clock: FC = () => {
 
-    let progressRing: any;
-    const incrementRing = () => {
-        if (progressRing) {
-            progressRing.style.background = `conic-gradient(
-                #212529 100deg,
-                lightgreen 100deg
-            )`
-        }
+    const [timer, setTimer] = useState({ hours: "00", minutes: "00", seconds: "00" });
+    const [controlText, setControlText] = useState("Start")
+
+    const updateTime = () => {
+        const progressRing = document.getElementById("timer-outer")! as HTMLElement;
+        chrome.storage.local.get(["hours", "minutes", "seconds", "isRunning", "setTime"], (res) => {
+            
+            if (res.isRunning) {
+                // Update clock time ui 
+                const hours: string = res.hours >= 10 ? res.hours.toString() : "0" + res.hours
+                const minutes: string = res.minutes >= 10 ? res.minutes.toString() : "0" + res.minutes
+                const seconds: string = res.seconds >= 10 ? res.seconds.toString() : "0" + res.seconds
+                setTimer({ hours, minutes, seconds })
+                
+                // Update clock gui
+                const curr_time: number = res.hours * 360 + res.minutes * 60 + res.seconds
+                const end_time: number = res.setTime.hours * 360 + res.setTime.minutes * 60
+                const degree: number = 360 - ((curr_time / end_time) * 360)
+                progressRing.style.background = `conic-gradient(
+                    lightgreen ${degree}deg,
+                    #212529 ${degree}deg 
+                )`
+            } 
+            // else {
+            //     // Update clock gui
+            //     progressRing.style.background = `conic-gradient(#212529 0deg, lightgreen 0deg )`
+            // }
+        })
     }
 
-    const [time, setTime] = useState({
-        hours: "00",
-        minutes: "00",
-        seconds: "00"
-    });
-   
-    // Initial re-render
-    useEffect(() => {
-        chrome.storage.local.get(["hours", "minutes"], (res) => {
-            if (res.hours != null && res.minutes != null) {
-                let hours = res.hours >= 10 ? res.hours.toString() : "0" + res.hours
-                let minutes = res.minutes >= 10 ? res.minutes.toString() : "0" + res.minutes
-                setTime({
-                    ...time,
-                    hours,
-                    minutes
-                })
-            }
+    const setControlTextHandler = () => {
+        chrome.storage.local.get(["isRunning", "hours", "minutes", "seconds"], (res) => {
+            
+            // FIX: Improve way to check start and stop button 
+            if (!(res.hours == 0 && res.minutes == 0 && res.seconds == 0)) {
+                chrome.storage.local.set({ isRunning: !res.isRunning })
+                setControlText(res.isRunning ? "Pause" : "Start")
+            }  
         })
-    }, [])
+    }
+   
+    // Initial re-render (allow initial set timer to show up)
+    useEffect(() => { updateTime() }, [])
 
     // Note: Assumed that isRunning is true
     useEffect(() => {
-        chrome.storage.onChanged.addListener(function(changes, namespace) {
-            // console.log("Chrome storage local=", res.hours, res.minutes, res.seconds)
-          
-            if ("hours" in changes) {
-                let updateHours: number = changes.hours.newValue
-                let hours: string = updateHours >= 10 ? updateHours.toString() : "0" + updateHours
-                setTime({
-                    hours,
-                    minutes: "00",
-                    seconds: "00",
-                })
-            } else if ("minutes" in changes) {
-                let updateMinutes: number = changes.minutes.newValue
-                let minutes: string = updateMinutes >= 10 ? updateMinutes.toString() : "0" + updateMinutes
-                setTime({
-                    ...time,
-                    minutes,
-                    seconds: "00"
-                })
-            } else if ("seconds" in changes) {
-                let updateSeconds: number = changes.seconds.newValue
-                let seconds: string = updateSeconds >= 10 ? updateSeconds.toString() : "0" + updateSeconds;
-                setTime({
-                    ...time,
-                    seconds
-                })            
-            }
-        })
-        progressRing = document.getElementById("timer-outer");
-        incrementRing()
-    }, [time])
+        chrome.storage.onChanged.addListener(() => { updateTime() })
+    }, [])
 
     
 
@@ -75,14 +59,16 @@ const Clock: FC = (): JSX.Element => {
             <div id="timer-outer" className={styles.timerOuter}>
                 <div className={styles.timerRing}>
                     <div className={styles.timer}>
-                        <span id="hours">{time.hours}</span>
+                        <span id="hours">{timer.hours}</span>
                         <span>:</span>
-                        <span id="minutes">{time.minutes}</span>
+                        <span id="minutes">{timer.minutes}</span>
                         <span>:</span>
-                        <span id="seconds">{time.seconds}</span>
+                        <span id="seconds">{timer.seconds}</span>
                     </div>
                     {/* <!-- FIX: Change out with picture later  --> */}
-                    <div className={styles.timerControl} id="timer-control">Start</div>
+                    <div onClick={() => setControlTextHandler()} className={styles.timerControl} id="timer-control">
+                        {controlText}
+                    </div>
                 </div>
             </div>
         </div>
