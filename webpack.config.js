68c6c0097dabfe48b0cc728bf74d027a54b4920a
Eric Pineda <ericvpineda@berkeley.edu>
2023-01-31 19:49:03 -0800

    update -- misc

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,60 @@
 const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin')
+const HtmlPlugin = require('html-webpack-plugin')
+
 module.exports = {
     mode: 'development',
-    entry: './src/test.tsx', // Tells webpack where to start looking 
+    devtool: 'cheap-module-source-map',
+    // Tells webpack where to start looking
+    entry: {
+        index: path.resolve('src/index.tsx')
+    },  
     module: { // allow webpack to handle other file times
         rules: [{
             use: 'ts-loader', 
             test: /\.tsx?$/,
             exclude: /node_modules/,
-        }]
+        },
+        {
+            use: ['style-loader', 'css-loader'],
+            test: /\.css$/i
+        },
+        {
+            type: 'assest/resource',
+            test: /\.(jpg|jpeg|png|woff|woff2|eot|ttf|svg)$/
+        }
+    ]
     },
+    plugins: [
+        new CopyPlugin({
+            patterns: [{
+                from: path.resolve('src/manifest.json'),
+                to: path.resolve('dist')
+            }]
+        }),
+        ...getHtmlPlugins([
+            'index',
+        ]),
+    ],
     resolve: {
         extensions: ['.tsx', '.ts', '.js'] // What file types apply modules to
     },
     output: {
-        filename: 'index.js',
-        path: path.resolve(__dirname, 'dist')
-    }
+        filename: '[name].js',
+        path: path.resolve('dist')
+    },
+    optimization: {
+        splitChunks: {
+            chunks: 'all'
+        }
+    },
+    
+}
+
+function getHtmlPlugins(chunks) {
+    return chunks.map(chunk => new HtmlPlugin({
+        title: 'React Extensions',
+        filename: `${chunk}.html`,
+        chunks: [chunk]
+    }))
 }
