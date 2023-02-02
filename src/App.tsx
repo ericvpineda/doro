0eef0090ccde2d5412f97cba20c3c41f9f1e8206
Eric Pineda <ericvpineda@berkeley.edu>
2023-02-01 17:23:37 -0800

    updated -- return to settings/clock icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,35 @@
-import React, {FC, Fragment, useEffect, useState} from 'react'
-import UserInput from './Components/UserInput/UserInput/UserInput'
-import Timer from './Components/Timer/Timer/Timer'
-import styles from './App.module.css'
+import React, { FC, Fragment, useEffect, useState } from "react";
+import UserInput from "./Components/UserInput/UserInput/UserInput";
+import Timer from "./Components/Timer/Timer/Timer";
+import styles from "./App.module.css";
+import { GearFill, ArrowLeftCircle } from "react-bootstrap-icons";
 
 const App: FC = () => {
-    const [showTimer, setShowTimer] = useState(false); 
+  const [showTimer, setShowTimer] = useState(false);
 
-    // Set current window 
-    const onClickHandler = () => {
-        setShowTimer(!showTimer)
-    }
+  // Set current window
+  const onClickHandler = () => {
+    setShowTimer(!showTimer);
+  };
 
-    return (
-        <Fragment>
-            {!showTimer && <UserInput setShowTimerHandler={setShowTimer}></UserInput>}
-            {showTimer && <Timer/>}
-            {/* FIX: Update edit button  */}
-            <button onClick={onClickHandler} className={styles.editBtn + " btn btn-primary"}>Edit</button>
-        </Fragment>
-    )
-} 
+  return (
+    <Fragment>
+      {!showTimer && <UserInput setShowTimerHandler={setShowTimer}></UserInput>}
+      {showTimer && <Timer />}
+      {/* FIX: Update edit button  */}
+      {showTimer ? (
+        <GearFill
+          onClick={onClickHandler}
+          className={styles.toEditButton}
+        ></GearFill>
+      ) : (
+        <ArrowLeftCircle
+          onClick={onClickHandler}
+          className={styles.toClockButton}
+        ></ArrowLeftCircle>
+      )}
+    </Fragment>
+  );
+};
 
-export default App; 
+export default App;
