20a1f5e777863754bfa56443f13a3076f453bbef
Eric Pineda <ericvpineda@berkeley.edu>
2023-02-10 12:40:22 -0800

    implemented -- auto refresh token working

--- src/Components/Login/Login.tsx
+++ src/Components/Login/Login.tsx
@@ -1,58 +1,108 @@
-import React, { FC, useState } from "react";
+import React, { FC, useEffect, useState } from "react";
 import { Spotify } from "react-bootstrap-icons";
 import styles from "./Login.module.css";
-import {generateChallenge, createAuthURL, requestAccessToken} from "../Utils/challenge";
-// import useAuth from "../Hooks/useAuth";
+import {
+  generateChallenge,
+  createAuthURL,
+  requestAccessToken,
+  requestRefreshToken,
+} from "../Utils/challenge";
+
 const random = require("random-string-generator");
+const info = {
+    state: encodeURIComponent(random(43)),
+    authCode: "",
+    challenge: "",
+    verifier: "",
+    refreshToken: "",
+}
 
 interface Props {
   setAccessTokenHandler: (param: string) => void;
 }
 
+// idea
+// - move info out of component 
+//  - issue: still need to set variables before passing into fxns
+
 const Login: FC<Props> = (props) => {
-    const [signedIn, setSignedIn] = useState(false);
-    const [accessToken, setAccessToken] = React.useState("");
-    const [refreshToken, setRefreshToken] = React.useState("");
-    const [expiresIn, setExpiresIn] = React.useState("");
-    const client = {
-        clientID: encodeURIComponent("a1794c4b3ff54d829531b3941ecf5620"),
-        state: encodeURIComponent(random(43)),
-        scope: encodeURIComponent("user-read-private user-read-email"),
-        uri: chrome.identity.getRedirectURL(),
-        type: encodeURIComponent("code"),
-        dialog: encodeURIComponent("true"),
-        codeChallenge: "",
-        codeVerifier: "",
-        authCode: ""
+  const [signedIn, setSignedIn] = useState(false);
+  const [accessToken, setAccessToken] = useState("");
+  const [refreshToken, setRefreshToken] = useState("");
+  const [expiresIn, setExpiresIn] = useState(0);
+
+  const setAccessTokenHandler = (data: any) => {
+    setAccessToken(data.access_token);
+    setRefreshToken(data.refresh_token);
+    setExpiresIn(data.expires_in);
+    setSignedIn(true);
+    // props.setAccessTokenHandler(data.access_token);
+  };
+
+  const spotifyBtnHandler = () => {
+    if (!signedIn) {
+      const [challenge, verifier] = generateChallenge();
+      info.challenge = challenge;
+
+      // Prompt user authorization
+      chrome.identity.launchWebAuthFlow(
+        { url: createAuthURL(info), interactive: true },
+        (response) => {
+          if (chrome.runtime.lastError) {
+            console.log("error: " + chrome.runtime.lastError.message);
+            return;
+          }
+          if (response == null) {
+            console.log("error: response url is null");
+            return;
+          }
+          // Note: URL allows get param after query variable
+          const url = new URL(response);
+          if (
+            url.searchParams.has("error") ||
+            url.searchParams.get("state") !== info.state
+          ) {
+            console.log("error: access_denied");
+            return;
+          }
+
+          info.authCode = url.searchParams.get("code")!,
+          info.verifier = verifier
+          // Use authoization code to get access token
+          requestAccessToken(info)
+            .then((res) => res.json())
+            .then((data) => {
+              setAccessTokenHandler(data);
+            })
+            .catch((e) => console.log(e));
+        }
+      );
+    }
+  };
+
+  useEffect(() => {
+    if (!refreshToken || !expiresIn) {
+      return;
     }
 
-    const spotifyBtnHandler = () => {
-        const [codeChallenge, codeVerifier] = generateChallenge();
-        client.codeChallenge = codeChallenge;
-        const url = createAuthURL(client);
-        chrome.identity.launchWebAuthFlow({url, interactive: true}, (response) => {
-            if (chrome.runtime.lastError) {
-                console.log("error: " + chrome.runtime.lastError.message);
-                return;
-            }
-            if (response == null) {
-                console.log("error: response url is null");
-                return;
-            }
-            // Note: URL allows get param after query variable
-            const url = new URL(response)
-            if (url.searchParams.has("error") || url.searchParams.get("state") !== client.state) {
-                console.log("error: access_denied");
-                return;
-            }
-            client.authCode = url.searchParams.get("code")!
-            client.codeVerifier = codeVerifier;
-            requestAccessToken(client)
-        });
-    };
+    const timeout = setInterval(async () => {
+     info.refreshToken = refreshToken;
+
+      await requestRefreshToken(info)
+        .then((res) => res.json())
+        .then((data) => {
+          setAccessTokenHandler(data);
+        })
+        .catch((e) => console.log(e));
+    }, (expiresIn - 60) * 1000);
+    return () => clearTimeout(timeout);
+  }, [refreshToken, expiresIn]);
 
   return (
-    <Spotify onClick={spotifyBtnHandler} className={styles.spotifyButton}></Spotify>
+    <Spotify
+      onClick={spotifyBtnHandler}
+      className={styles.spotifyButton}
+    ></Spotify>
   );
 };
 
--- src/Components/Utils/challenge.tsx
+++ src/Components/Utils/challenge.tsx
@@ -1,8 +1,15 @@
 const SHA256 = require("crypto-js/sha256");
 const BASE64 = require("crypto-js/enc-base64");
 const random = require("random-string-generator");
+const client = {
+  id: encodeURIComponent("a1794c4b3ff54d829531b3941ecf5620"),
+  scope: encodeURIComponent("user-read-private user-read-email"),
+  uri: chrome.identity.getRedirectURL(),
+  type: encodeURIComponent("code"),
+  dialog: encodeURIComponent("true"),
+}
 
-const generateChallenge = () => {
+const generateChallenge = (): [string, string] => {
     const codeVerifier = random(64);
     const codeChallenge = BASE64.stringify(SHA256(codeVerifier))
         .replace(/\+/g, "_")
@@ -14,14 +21,14 @@ const generateChallenge = () => {
  // Build URL for request user authorization 
  const createAuthURL = (info: any): string => {
     const url = new URL("https://accounts.spotify.com/authorize");
-    url.searchParams.append("client_id", info.clientID);
+    url.searchParams.append("client_id", client.id);
     url.searchParams.append("state", info.state);
-    url.searchParams.append("scope", info.scope);
-    url.searchParams.append("redirect_uri", info.uri);
-    url.searchParams.append("response_type", info.type);
-    url.searchParams.append("show_dialog", info.dialog);
+    url.searchParams.append("scope", client.scope);
+    url.searchParams.append("redirect_uri", client.uri);
+    url.searchParams.append("response_type", client.type);
+    url.searchParams.append("show_dialog", client.dialog);
     url.searchParams.append("code_challenge_method", "S256");
-    url.searchParams.append("code_challenge", info.codeChallenge);
+    url.searchParams.append("code_challenge", info.challenge);
     return url.href;
   };
 
@@ -30,20 +37,29 @@ const generateChallenge = () => {
     const url = new URL("https://accounts.spotify.com/api/token");
     url.searchParams.append("grant_type", "authorization_code");
     url.searchParams.append("code", info.authCode);
-    url.searchParams.append("code_verifier", info.codeVerifier);
-    url.searchParams.append("redirect_uri", info.uri);
-    url.searchParams.append("client_id", info.clientID);
+    url.searchParams.append("code_verifier", info.verifier);
+    url.searchParams.append("redirect_uri", client.uri);
+    url.searchParams.append("client_id", client.id);
     const params = new URLSearchParams(url.search);
     const headers = {
       "Content-Type": "application/x-www-form-urlencoded",
     };
 
       // Post request to get access tokenx
-    const response = await fetch(url.href, {method: "POST", headers, body: params.toString()}).
-        then((res) => res.json())
-        .then((data) => console.log(data))
-        .catch((e) => console.log(e));
+    return await fetch(url.href, {method: "POST", headers, body: params.toString()})
 }; 
+    
+    const requestRefreshToken = async (info: any) => {
+        const url = new URL("https://accounts.spotify.com/api/token");
+        url.searchParams.append("grant_type", "refresh_token");
+        url.searchParams.append("refresh_token", info.refreshToken);
+        url.searchParams.append("client_id", client.id);
+        const params = new URLSearchParams(url.search);
+        const headers = {
+            "Content-Type": "application/x-www-form-urlencoded",
+        };
+        return await fetch(url.href, {method: "POST", headers, body: params.toString()})
+    }
 
 
-export {generateChallenge, createAuthURL, requestAccessToken};
+export {generateChallenge, createAuthURL, requestAccessToken, requestRefreshToken};
